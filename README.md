b14b255ca307d076727a146021037be0f59f0f2a
unknown <wiktor.sterczala.xd@gmail.com>
2024-01-15 22:00:30 +0100

    dodano funkcję znajdującą min i max funkcji, funkcja calka_monte_carlo() zwraca przybliżoną wartość całki oznaczonej funkcji w ciągu 10-20 sekund. Dopisano do README.md wymagania do umożliwienia implementacji funkcji.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SystemOperacjiNaFunkcjachPWI
 Projekt na przedmiot Podstawowy warsztat informatyka
+
+Specyfikacja do funkcji calka_monte_carlo : wymagane pobranie modułu sympy komendą:
+pip install sympy
--- calka_monte_carlo.py
+++ calka_monte_carlo.py
@@ -1,7 +1,7 @@
-import random 
+import random, math 
 from sympy import *
 
-def calka_monte_carlo(funkcja, a, b):
+def minmax(funkcja,a,b):
     x = symbols('x')
     wyrazenie = sympify(funkcja)
     x_first = a
@@ -9,15 +9,32 @@ def calka_monte_carlo(funkcja, a, b):
     pochodna = diff(funkcja, x)
     kres_a = wyrazenie.subs(x, x_first)
     kres_b = wyrazenie.subs(x, x_last)
-    punkty_krytyczne = [kres_a, kres_b]
+    punkty_krytyczne = [kres_b, kres_a]
     miejsca_zerowe_pochodnej = solve(pochodna, x)
     for i in miejsca_zerowe_pochodnej:
-        punkty_krytyczne.append(wyrazenie.subs(x, i))
-    max = max(punkty_krytyczne)
-    min = min(punkty_krytyczne)
-    
+        if i<=b and i>=a:
+            punkty_krytyczne.append(wyrazenie.subs(x, i))
+    maximum = max(punkty_krytyczne)
+    minimum = min(punkty_krytyczne)
+    return (minimum, maximum)
 
-
-    return (miejsca_zerowe_pochodnej, pochodna)
-
-print(calka_monte_carlo('5*x**2 - 3*x + 2',2,5))
+def calka_monte_carlo(funkcja, a, b):
+    minimum = minmax(funkcja,a,b)[0]
+    maximum = minmax(funkcja,a,b)[1]
+    if minimum > 0:
+        minimum = 0
+    x = symbols('x')
+    cnt = 0
+    wyrazenie = sympify(funkcja)
+    przybliżenie = 10**4
+    for i in range(przybliżenie):
+        x_p = random.uniform(a,b)
+        y_p = random.uniform(minimum, maximum)
+        if y_p <= wyrazenie.subs(x,x_p):
+            cnt+=1
+    posibility = cnt/przybliżenie
+    pole = (maximum-minimum) *(b-a)
+    calka = posibility * pole
+    return calka
+        
+print(calka_monte_carlo('sin(x)',2,5))
