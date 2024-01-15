c1dfb6af8360775eb03dc020da77d27832d71ab4
unknown <wiktor.sterczala.xd@gmail.com>
2024-01-15 16:58:03 +0100

    Dodano operację liczenia min i max z funkcji na wskazanym przedziale

--- calka_monte_carlo.py
+++ calka_monte_carlo.py
@@ -1,12 +1,23 @@
 import random 
-from sympy import symbols, sympify,diff
+from sympy import *
 
 def calka_monte_carlo(funkcja, a, b):
     x = symbols('x')
     wyrazenie = sympify(funkcja)
-    x_value = a
-    wynik = wyrazenie.subs(x, x_value)
+    x_first = a
+    x_last = b
     pochodna = diff(funkcja, x)
-    return wynik, pochodna
+    kres_a = wyrazenie.subs(x, x_first)
+    kres_b = wyrazenie.subs(x, x_last)
+    punkty_krytyczne = [kres_a, kres_b]
+    miejsca_zerowe_pochodnej = solve(pochodna, x)
+    for i in miejsca_zerowe_pochodnej:
+        punkty_krytyczne.append(wyrazenie.subs(x, i))
+    max = max(punkty_krytyczne)
+    min = min(punkty_krytyczne)
+    
+
+
+    return (miejsca_zerowe_pochodnej, pochodna)
 
 print(calka_monte_carlo('5*x**2 - 3*x + 2',2,5))
