1fc7ae15728c83370c7ad75879c2f3b56568d238
dJedrasStudent <152498070+dJedrasStudent@users.noreply.github.com>
2024-01-17 21:21:53 +0100

    ulepszenie calka_num.py

--- calka_num.py
+++ calka_num.py
@@ -5,7 +5,7 @@ def calka_num(funkcja, a, b):
     dokladnosc = 10**5
     calka = 0
     przedzial = (b-a)/dokladnosc
-    x = a
+    x = a + przedzial/2
     for i in range(dokladnosc):
         wartosc = eval(funkcja)
         calka += wartosc*przedzial
